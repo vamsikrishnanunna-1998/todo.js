e162dc187dec7b60a521f3b52737595cec39c192
vamsikrishnanunna-1998 <nunnavamsikrishna1998@msitprogram.net>
2020-05-15 17:47:22 +0530

    added date, checkbox

--- todo-app.js
+++ todo-app.js
@@ -9,17 +9,37 @@ class Task {
     }
 
     toString() {
-        let htmlText = '<li class="task" ><div>'
+        let htmlText = '<li class="task" id='+this.taskId+'><div>'
+        var dummy = false;
+
         htmlText += this.name
-        htmlText += ", " + this.dueDate.getDate() 
-                 + "/" + this.dueDate.getMonth();
-        htmlText += '<input type="checkbox" name="isDone" id="isDone">'
+        htmlText += ", " + this.dueDate;
+        htmlText += '<input type="checkbox" onclick="myfunction('+this.taskId+')" name="isDone" id="isDone">'
         htmlText += '<button onclick="deleteTask(';
         htmlText += this.taskId;
         htmlText += ')">Delete</button>';
         htmlText += '</div></li>';
+        // if (dummy) {
+        //     console.log(dummy+ "in class");
+        //     return htmlText.strike();
+        // }
         return htmlText;
     }
+}
+function myfunction(checkId) {
+    // this.isDone = true;
+    // dummy = true;
+    console.log(checkId +" my function");
+    taskList.forEach((task) => {
+        // listUI.innerHTML += task.toString();
+        if (checkId == task.taskId ) {
+            task.isDone = true;
+
+             
+        }
+        render()
+       
+    })
 }
 
 function render() {
@@ -28,8 +48,17 @@ function render() {
     if (taskList.length === 0) listUI.innerHTML = "No tasks todo :-)"
     taskList.forEach((task) => {
         listUI.innerHTML += task.toString();
+        // console.log(task.isDone+" testing ");
+        var checkBox = document.getElementById("isDone");
+        console.log(checkBox.checked);
+        var text = document.getElementById(task.taskId);
+        if (task.isDone == true) {
+            document.getElementById(task.taskId).style.color = "blue";
+        }
     })
+    
 }
+
 
 function deleteTask(taskId) {
     taskList = taskList.filter(
@@ -47,7 +76,8 @@ function deleteTask(taskId) {
 
 function createTask() {
     const taskName = document.getElementById("taskName").value;
-    addTask(new Task(taskName, new Date(), false));
+    const inputDueDate = document.getElementById("inputDueDate").value;
+    addTask(new Task(taskName, inputDueDate, false));
 }
 
 function addTask(t) {
@@ -66,7 +96,7 @@ function init() {
     // assign it to taskList
     // render
 
-    task = new Task("welcome task", new Date("May 30, 2020"), false);
+    task = new Task("welcome task", new Date("May, 30, 2020"), false);
     addTask(task);
     console.log(task);
 }
